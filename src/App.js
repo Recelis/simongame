9097aa2b04d75095f51b1d04c6856ed1719b4e04
JackyRecelis <jackymlui@live.com>
2017-06-12 23:11:18 +1000

    made changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class button extends Component{
+
+}
+
+class board extends Component{
+
+}
+
+class Game extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+          <h2>Simon Game</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button></button>
       </div>
     );
   }
 }
 
-export default App;
+export default Game;
