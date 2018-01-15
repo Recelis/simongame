789c65528078f9f113a5a6b8757a21daa943bbcf
Jacky <jackymlui@live.com>
2018-01-15 16:13:30 +1000

    slight redesign of css, github button added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import MyGame from './components/myGame'
-
+import github from "./images/github.png";
 class App extends Component {
+  openWindow(){
+    window.open("https://github.com/Recelis/simongame",'_blank');
+  }
   render() {
     return (
       <div>
         <MyGame/>
-        <div><p className = "copyright">created By Jacky Lui using ReactJS &#169; 2017</p></div>
+        <p className = "copyright">created By Jacky Lui using ReactJS &#169; 2017
+          <span><button onClick = {()=>this.openWindow()}className = "github"><img className = "github" src = {github}></img></button></span>
+        </p>
       </div>
     )
   }
